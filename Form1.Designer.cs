ac517f05d420e134b0adbae066e8391d2e295c4f
MastianPlayZ <maximilian.zettler@dm.de>
2018-05-18 11:54:46 +0200

    -

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -106,7 +106,6 @@
             this.Controls.Add(this.lbl_Code);
             this.Controls.Add(this.btn_Code);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Authenticator";
